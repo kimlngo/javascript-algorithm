fe98cb4524d1999703955d7ba3b9774570302ca1
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-11-01 12:42:03 -0400

    Implement countingUniqueValues by using two pointers

--- multiPointer.js
+++ multiPointer.js
@@ -20,3 +20,27 @@ function sumZero(arr) {
 
   return undefined;
 }
+
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(countUniqueValues([1, 1, 1, 1, 1]));
+console.log(countUniqueValues([]));
+console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  else if (arr.length === 1) return 1;
+
+  let count = 1;
+  let pre = 0,
+    cur = 1;
+
+  while (cur < arr.length) {
+    if (arr[pre] < arr[cur]) {
+      count++;
+    }
+    pre = cur;
+    cur++;
+  }
+
+  return count;
+}
