23ab095e562783380a082183faed53d786445942
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-03-19 16:57:05 -0400

    Remove element implementation

---
+++ leetcode/RemoveElement.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function removeElement(nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  console.log(nums.join(' -> '));
+  return k;
+}
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+console.log(removeElement([3, 2, 2, 3], 3));
