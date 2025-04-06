51ff99c2e364482fb90dbd56e51ff6ad89a8256a
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-04-06 15:38:29 -0400

    Convert Sorted Array to BST

---
+++ leetcode/BuildBSTFromSortedArray.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import { TreeNode } from './data/TreeNode.js';
+
+function sortedArrayToBST(nums) {
+  const length = nums.length;
+
+  if (length === 0) return null;
+  else if (length === 1) return new TreeNode(nums[0]);
+
+  let root;
+  if (length === 2) {
+    root = new TreeNode(nums[1]);
+    root.left = new TreeNode(nums[0]);
+  } else if (length === 3) {
+    root = new TreeNode(nums[1]);
+    root.left = new TreeNode(nums[0]);
+    root.right = new TreeNode(nums[2]);
+  } else {
+    // more than 3 elements
+    const mid = Math.trunc(length / 2);
+    root = new TreeNode(nums[mid]);
+    root.left = sortedArrayToBST(nums.slice(0, mid));
+    root.right = sortedArrayToBST(nums.slice(mid + 1, length));
+  }
+
+  return root;
+}
+
+const arrs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(sortedArrayToBST(arrs));
