beccbcd294498c70722486a9718507ece60d9de6
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-12-06 15:08:21 -0500

    Remove Adjacent Duplicates

---
+++ leetcode/apple/RemoveAdjacentDuplicates.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function (s) {
+  if (s.length === 1) return s;
+
+  const stack = [];
+  stack.push(s[0]);
+
+  for (let i = 1; i < s.length; i++) {
+    const c = s[i];
+
+    if (stack.length === 0) {
+      stack.push(c);
+      continue;
+    }
+
+    const pop = stack.pop();
+
+    if (pop !== c) {
+      stack.push(pop);
+      stack.push(c);
+    }
+  }
+
+  return stack.join('');
+};
+
+console.log(removeDuplicates('aaaaaaaaa'));
